b685e7a683c956b8a5434ab7484fba5cd0fe7c30
aamina12 <57256481+aamina12@users.noreply.github.com>
2020-11-28 15:11:09 +0100

    Delete VictoryCard.java

--- VictoryCard.java
+++
@@ -1,8 +0,0 @@
-package shape_up;
-
-public class VictoryCard extends Carte{
-
-	public VictoryCard(String couleur, String forme, boolean remplissage, int num) {
-		super(couleur, forme, remplissage, num);
-	}
-}
